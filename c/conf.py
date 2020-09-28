199fb54c8ba6e12f107ad0e0e1990be386440567
Adrian Jackson <adrianj@epcc.ed.ac.uk>
2020-09-28 10:28:35 +0100

    Updating dates

--- c/conf.py
+++ c/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'HDF5 Tutorial'
-copyright = u'2014, The University of Edinburgh'
+copyright = u'2020, The University of Edinburgh'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
